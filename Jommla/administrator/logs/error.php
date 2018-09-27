a869f71e67649ca57bd3c97bde769b066ba428fa
Dulce Sustaita <1530123@upv.edu.mx>
2018-09-26 23:25:05 -0500

    Initial commit

---
+++ Jommla/administrator/logs/error.php
@@ -0,0 +1,7 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2018-09-27 03:08:08 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2018-09-27T03:08:08+00:00	INFO ::1	joomlafailure	Empty password not allowed.
